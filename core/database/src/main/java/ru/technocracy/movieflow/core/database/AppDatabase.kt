150650eace201319926945ab8a9b975fb86492fd
kkruchock <mosk.bima@gmail.com>
2026-05-28 01:45:09 +0300

    feat: user actions database layer

--- core/database/src/main/java/ru/technocracy/movieflow/core/database/AppDatabase.kt
+++ core/database/src/main/java/ru/technocracy/movieflow/core/database/AppDatabase.kt
@@ -5,15 +5,17 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import ru.technocracy.movieflow.core.database.converter.ListConverters
 import ru.technocracy.movieflow.core.database.dao.MovieDao
+import ru.technocracy.movieflow.core.database.dao.UserMovieDao
 import ru.technocracy.movieflow.core.database.entity.MovieEntity
 
 @Database(
     entities = [MovieEntity::class],
-    version = 1, exportSchema = false
+    version = 2, exportSchema = false
 )
 @TypeConverters(
     ListConverters::class
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
+    abstract fun userMovieDao(): UserMovieDao
 }
